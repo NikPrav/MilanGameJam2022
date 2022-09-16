479e7f4104305885d4a3f92267bc0618440ff0c0
nikhil <nikhil.pravin@gmail.com>
2022-09-17 02:04:43 +0530

    Bunker Ending Fixed

--- Levels/BunkerLevel.gd
+++ Levels/BunkerLevel.gd
@@ -8,6 +8,6 @@ func _ready():
 func _on_LevelTrigger_body_entered(body):
 
 	if(body.is_in_group("player")):
-		get_tree().change_scene("res://Levels/BunkerLevel.tscn")
+		get_tree().change_scene("res://UI/Ending.tscn")
 
 
