81908e2a2a7f812e364522566c9160fc360c4c61
Anirudh Raghav <anirudhraghav7101@gmail.com>
2022-09-17 01:55:24 +0530

    omg we got this shit done

--- Levels/LabLevel.gd
+++ Levels/LabLevel.gd
@@ -6,5 +6,10 @@ func _physics_process(delta):
 	cur_layer = get_node("Player").cur_layer
 
 
-func _on_LevelTrigger_area_entered(area):
-	get_tree().change_scene("res://Levels/BunkerLevel.tscn")
+#func _on_LevelTrigger_area_entered(area):
+#	get_tree().change_scene("res://Levels/BunkerLevel.tscn")
+
+
+func _on_LevelTrigger_body_entered(body):
+	if(body.is_in_group("player")):
+		get_tree().change_scene("res://Levels/BunkerLevel.tscn")
--- Levels/LevelTriggerBunker.gd
+++ Levels/LevelTriggerBunker.gd
@@ -1,13 +1,6 @@
-extends "res://Levels/Level1.gd"
-
-
-func _ready():
-	pass
+extends Area2D
 
 
 func _on_LevelTrigger_body_entered(body):
-
 	if(body.is_in_group("player")):
 		get_tree().change_scene("res://Levels/BunkerLevel.tscn")
-
-
