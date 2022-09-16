4dfc8e98887ab90cdb83195d3d85cfc82202b6c3
Anirudh Raghav <anirudhraghav7101@gmail.com>
2022-09-17 01:09:53 +0530

    Fixed UI issues

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -4,10 +4,14 @@ var coins = 0
 var lives = 3
 
 func _ready():
-	$CoinCount.text = str(coins)
-	$LivesCount.text = str(lives)
+	$CoinsPanel/CoinCount.text = String(coins)
+	$LivesPanel/LivesCount.text = String(lives)
 
 
 func _on_coin_collected():
 	coins += 1
+	_ready()
+
+func _on_lives_lost():
+	lives -= 1
 	_ready()
--- UI/TitleScreen.gd
+++ UI/TitleScreen.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _physics_process(delta):
+	if(Input.is_action_just_pressed("continue")):
+		get_tree().change_scene("res://UI/StoryScene.tscn")
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://UI/StoryScene1.tscn")
+	get_tree().change_scene("res://UI/StoryScene.tscn")
